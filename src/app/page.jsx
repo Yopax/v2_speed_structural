a984aaa51a053fed61fe21f447b4b0d0670cdfab
Darli Barreto <n00167313@upn.pe>
2024-05-13 18:45:54 -0500

    07. LP terminada con responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
 "use client";
+import Footer from '@/components/01_intefaz_calculos/Footer';
 import NavBarCalculo from '@/components/01_intefaz_calculos/NavBarCalculo'
 import Descripcion from '@/components/02_intefaz-landingpage/Descripcion';
+import MyModal from '@/components/02_intefaz-landingpage/MyModal';
 import PresentacionHome from '@/components/02_intefaz-landingpage/PresentacionHome';
 import React from 'react'
 
@@ -10,6 +12,8 @@ function page() {
      <NavBarCalculo />
      <PresentacionHome />
      <Descripcion />
+     <MyModal />
+     <Footer />
     </>
   )
 }
